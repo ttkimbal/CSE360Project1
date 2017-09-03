350ae56f570a30ea2d6636c9cf96def50fb05dba
Jnathenson96 <31576794+Jnathenson96@users.noreply.github.com>
2017-09-02 17:07:27 -0700

    Update Tutor.java
    Basic workings of the tutor panel, loads html files successfully, still requires commenting and refining the code

--- Tutor.java
+++ Tutor.java
@@ -2,22 +2,44 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import javax.swing.text.html.HTMLEditorKit;
+
 import java.io.*;
 
 public class Tutor extends JPanel
 {
     int state;
+    JLabel name  = new JLabel("Jared Nathenson");
+    JEditorPane ep = new JEditorPane();
+    
 
     public Tutor()
     {
         state = 0;
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        add(new JLabel("Jared Nathenson"), c);
+        add(name, c);
+        ep.setEditorKit(new HTMLEditorKit());
+ 
     }
 
-    public void changeState(int state2)
+    public void changeState(int status)
     {
-
+    	remove(name);
+    	String filename = "P" + status + ".html";
+    	FileReader r = null;
+		try {
+			r = new FileReader(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	try {
+			ep.read(r, filename);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	add(ep); 	
     }
 }
